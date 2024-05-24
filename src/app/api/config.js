803c2d230c02319fe27132ef776efc7010d11904
heraMMXXIII <vadimtop228pc@gmail.com>
2024-05-24 14:05:13 +0300

    Update config.js

--- src/app/api/config.js
+++ src/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend-vadim.nomoredomainswork.ru/' 
+export const BASE_URL = 'https://pindie-backend-vadim.nomoredomainswork.ru/api' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
